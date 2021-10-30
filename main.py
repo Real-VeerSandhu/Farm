a9b6b8106d0e715f37d9ce25d4809d708fe22714
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-10-30 17:57:16 -0400

    #29507

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import time
 import os
 import cultivate
 
-def main():
-    for i in range(3):
+def main(amount):
+    for i in range(amount):
         planted_crop = cultivate.plant()
-        print('-'*50)
+        print('\n', '-'*50)
         os.system(f'git commit -am "#{planted_crop}"')
         os.system('git push --all')
         time.sleep(0.1)
 
+crop_amount = int(input('Crop Amount: '))
     
 if __name__ == '__main__':
-    main()
+    main(crop_amount)
