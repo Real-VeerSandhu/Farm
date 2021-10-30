2fc3a6ab98a1467c38e600ec69f85530221518d7
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-10-30 00:15:54 -0400

    #90291

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import cultivate
 def main():
     for i in range(3):
         planted_crop = cultivate.plant()
+        print('-'*50)
         os.system(f'git commit -am "#{planted_crop}"')
         os.system('git push --all')
-        time.sleep(3)
+        print('-'*50)
+        time.sleep(0.1)
 
     
 if __name__ == '__main__':
