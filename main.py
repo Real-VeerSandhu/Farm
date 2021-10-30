59398ff374ae4b182e9e1ac6faf468270608558d
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-10-30 00:13:16 -0400

    #28141

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import time
-import harvest
+import os
+import cultivate
 
 def main():
-    harvest.plant()
+    for i in range(3):
+        planted_crop = cultivate.plant()
+        os.system(f'git commit -am "#{planted_crop}"')
+        os.system('git push --all')
+        time.sleep(3)
 
+    
 if __name__ == '__main__':
     main()
