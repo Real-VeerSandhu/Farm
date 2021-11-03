86b622e7286a1f7f0920c430fc70d1240006470e
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-11-02 22:15:10 -0400

    seed id

--- cultivate.py
+++ cultivate.py
@@ -1,7 +1,7 @@
 from random import randint
 
 def plant():
-    crop = randint(0, 99999) # Number Id
+    crop = randint(10000, 99999) # Number Id
     
     land = open('dump/land.txt', 'a')
     land.write(f'\n{crop}')
