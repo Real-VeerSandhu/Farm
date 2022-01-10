0382844806069e8c02980e7fb9e5492016d94f98
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-01-09 23:58:10 -0500

    delay

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import cultivate
 
 def main(amount):
     for i in range(amount):
-        planted_crop = cultivate.plant()
+        planted_crop = cultivate.plant() # Get new crop
         print('\n', '-'*50)
-        # os.system(f'git commit -am "#{planted_crop}"')
-        # os.system('git push --all')
-        time.sleep(0.1)
+        os.system(f'git commit -am "#{planted_crop}"')
+        os.system('git push --all')
+        time.sleep(0.1) # Delay to prevent read-write overlap
 
 crop_amount = int(input('Crop Amount: '))
     
